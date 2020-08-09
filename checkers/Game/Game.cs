5c0589482ef73aaf51c31b006585773d2267da62
zmertens <zmertens@asu.edu>
2020-08-09 08:01:08 -0700

    Add some class members to the game but leave them as comments/TODOs

--- checkers/Game/Game.cs
+++ checkers/Game/Game.cs
@@ -8,6 +8,17 @@ namespace Game
     {
         RenderWindow window;
 
+        // static ScoreKeeper keeper = new ScoreKeeper();
+        
+        // static Board CheckerBoard = new Board();
+        // Player[2] Players;
+
+
+
+        
+
+
+
         public SfmlApplication(string title, UInt16 x, UInt16 y) {
             window = new RenderWindow(new VideoMode(x, y), title);
 
